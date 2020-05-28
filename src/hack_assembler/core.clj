64ba39e15d9feb6d1bf2e7daa420ae02ac6fa8ea
James Borden <jmborden@gmail.com>
2020-05-28 00:00:53 -0400

    assembler can handle pre-defined symbols and goto labels

--- src/hack_assembler/core.clj
+++ src/hack_assembler/core.clj
@@ -67,13 +67,42 @@
    "D&A" "0" "D&M" "1"
    "D|A" "0" "D|M" "1"})
 
+;; @i starts at 15
+(def predefined-symbols
+  (merge
+   (apply merge (map #(hash-map (str "R" %) (str %)) (range 0 16)))
+   {"SCREEN" "16384"
+    "KBD" "24576"
+    "SP" "0"
+    "LCL" "1"
+    "ARG" "2"
+    "THIS" "3"
+    "THAT" "4"}))
+
+;; second pass - add var symbols
+
+;; first pass - add label symbols
+(defn first-pass
+  [tokens]
+  (->> tokens
+       (map-indexed (fn [idx itm]
+                      (when (= (first itm) :LABEL_SYMBOL)
+                        [(-> itm second second) idx])))
+       (into [])
+       (filterv (comp not nil?))
+       ;; increment it by its position
+       (map-indexed (fn [idx itm]
+                      [(first itm) (str (- (second itm) idx))]))
+       (mapv (partial apply hash-map))
+       (apply merge)))
+
 ;; https://stackoverflow.com/questions/21448788/printing-the-binary-value-of-a-number-in-clojure
-(defn print-bits [b]
+(defn print-bits
   "Convert integer to string representation in binary, giving two's complement negative numbers"
+  [b]
   (let [class-name (.getName (class b))
         is-byte (= "java.lang.Byte" class-name)
         num-bits (clojure.lang.Reflector/getStaticField class-name "SIZE")
-        format-string (str "~" num-bits "'0b")
         bin-str-fn #(clojure.lang.Reflector/invokeStaticMethod
                      (if is-byte "java.lang.Integer" class-name)
                      "toBinaryString"
@@ -91,12 +120,15 @@
 
 (defn a-instruction->binary-string
   "Given a vector representing an a-instruction, convert it to a binary string"
-  [v]
-  (->> v rest (into (sorted-map)) :VALUE int-string->16-bit-string))
+  [v env]
+  (let [value (->> v rest (into (sorted-map)) :VALUE)]
+    (if (re-matches #"\d+" value)
+      (int-string->16-bit-string value)
+      (int-string->16-bit-string (get env value)))))
 
 (defn c-instruction->binary-string
-  [v]
   "Given a vector representing a c-instruction, convert it to a binary string"
+  [v]
   (let [{:keys [COMP DEST JUMP]} (->> v rest (into (sorted-map)))]
     (str "111"
          (get comp->abit COMP)
@@ -105,13 +137,13 @@
          (get jump->jbits JUMP))))
 
 (defn tokens->binary-string
-  [tokens]
+  [tokens env]
   "Given a set of tokens, convert them to their binary string representation for the hack computer"
   (map #(let [instruction-type (first %)]
-          (if (= :A_INSTRUCTION instruction-type)
-            (a-instruction->binary-string %)
-            (c-instruction->binary-string %)))
-       tokens))
+          (condp = instruction-type
+            :A_INSTRUCTION (a-instruction->binary-string % env)
+            :C_INSTRUCTION (c-instruction->binary-string %)))
+       (filterv #(not= (first %) :LABEL_SYMBOL) tokens)))
 
 (defn tokenize
   "Given a string, tokenize it"
@@ -124,5 +156,14 @@
   (let [io-file (io/file filename)
         filepath (.getPath io-file)
         output-filename (string/replace filepath #".asm" ".hack")
-        input (slurp filepath)]
-    (spit output-filename (-> input tokenize rest (into []) tokens->binary-string (->> (string/join "\n")) (str "\n")))))
+        input (slurp filepath)
+        tokens (->> input tokenize rest (into []))
+        labels-symbols (first-pass tokens)
+        env (merge labels-symbols predefined-symbols)]
+    (spit output-filename
+          (-> input tokenize
+              rest
+              (into [])
+              (tokens->binary-string env)
+              (->> (string/join "\n"))
+              (str "\n")))))
